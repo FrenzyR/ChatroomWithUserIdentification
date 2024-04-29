b36d8f28d8239fca128dde429a4aebab53164481
FrenzyR <rubenverrod@gmail.com>
2024-04-29 21:59:29 +0200

    Added basic server functionality, aswell as the bones for the first two forms

---
+++ BasicServerFunctionality/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicServerFunctionality
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Forms/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Forms
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
